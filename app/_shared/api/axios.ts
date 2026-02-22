5a31a4465961ebf28f1104fc1e379003145f9f6e
prantik009 <biswasritam5.13@gmail.com>
2026-02-22 23:40:37 +0530

    updated the backend url

--- app/_shared/api/axios.ts
+++ app/_shared/api/axios.ts
@@ -4,60 +4,58 @@ import axios from "axios";
 import { refreshToken } from "./auth.api";
 import { useAuthStore } from "../store/auth.store";
 import { getCartSessionId } from "../utils/cartSession";
+import { ENV } from "@/app/config/env";
 
 let accessToken: string | null = null;
 
 export const setAccessToken = (token: string | null) => {
-    accessToken = token;
+  accessToken = token;
 };
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    withCredentials: true,
+  baseURL: `${ENV.API_URL}`,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-    config.headers["x-session-id"] = getCartSessionId();
+  config.headers["x-session-id"] = getCartSessionId();
 
-    if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-    }
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
 
-    return config;
+  return config;
 });
 
 api.interceptors.response.use(
-    (res) => res,
-    async (error) => {
-        const originalRequest = error.config;
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
 
-        if (
-            error.response?.status === 401 &&
-            !originalRequest._retry &&
-            !originalRequest.url?.includes("/auth/refresh") // 🔥 prevent self-refresh
-        ) {
-            originalRequest._retry = true;
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url?.includes("/auth/refresh") // 🔥 prevent self-refresh
+    ) {
+      originalRequest._retry = true;
 
-            try {
-                const { data } = await refreshToken();
+      try {
+        const { data } = await refreshToken();
 
-                setAccessToken(data.accessToken);
-                useAuthStore.getState().login(data.user);
+        setAccessToken(data.accessToken);
+        useAuthStore.getState().login(data.user);
 
-                originalRequest.headers.Authorization =
-                    `Bearer ${data.accessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
 
-                return api(originalRequest);
-            } catch {
-                useAuthStore.getState().logout(); // 🔥 important
-                return Promise.reject(error);
-            }
-        }
-
+        return api(originalRequest);
+      } catch {
+        useAuthStore.getState().logout(); // 🔥 important
         return Promise.reject(error);
+      }
     }
+
+    return Promise.reject(error);
+  },
 );
 
-
 export default api;
-
